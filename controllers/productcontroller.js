087ebe35985f2921779286c1cc093e8e1ade82af
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2023-09-20 00:40:34 +0530

    added delete and product update functionality

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -33,6 +33,48 @@ var productController=(app)=>
         }
     })
 
+    app.delete('/product/:id',async function(req,res)
+    {
+        try{
+            var id=req.params.id;
+            var product=await Product.findByIdAndDelete(id);
+            if(!product)
+            {
+                res.status(404).json({message:`can not find any product with Id ${id}`});
+            }
+            else{
+                res.status(200).json(product);
+
+            }
+          
+
+        }catch(err)
+        {
+            console.log(err.message);
+            res.status(500).json({message:err.message});
+        }
+    })
+
+    app.put('/product/:id',async function(req,res)
+    {
+        try{
+            var product=await Product.findByIdAndUpdate(req.params.id,req.body,{new:true});
+            if(!product)
+            {
+                res.status(404).json({message:`can not find any product with Id ${id}`});
+            }
+            else{
+                res.status(200).json(product);
+
+            }
+            
+        }catch(err)
+        {
+            console.log(err.message);
+            return res.status(500).json({message:err.message});
+        }
+    });
+
 }
 
 module.exports=productController;
